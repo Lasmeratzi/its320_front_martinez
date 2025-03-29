ee98ad4dc44f986970a588962c6ce3cbc921028b
Lasmeratzi <ralfmatthew12@gmail.com>
2025-03-29 15:51:23 +0800

    forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TodolistComponent } from './todolist/todolist.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [TodolistComponent],
+  template: `<app-todolist></app-todolist>`,
 })
-export class AppComponent {
-  title = 'midterm';
-}
+export class AppComponent {}
